5a4bc9001785e6b0771e426cbf246a4f670e5fe8
tj924 <54832873+tj924@users.noreply.github.com>
2020-04-22 18:16:39 -0400

    Assign 6 final

---
+++ module6/d3_lab/01answer.js
@@ -0,0 +1,29 @@
+d3.csv("ue_industry.csv", data => {
+    // Define your scales and generator here.
+
+    const xScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => +d.index))
+        .range([20, 1180]);
+
+    const yScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => +d.Agriculture))
+        .range([580, 20]);
+
+    const dataset = data
+        .filter(row => Number.isInteger(+row.Agriculture | NaN))
+        .map(row =>({
+                "x": +row.index,
+                "y": +row.Agriculture
+        }));
+
+    let line = d3.line()
+        .x(d => xScale(+d.x))
+        .y(d => yScale(+d.y));
+
+    d3.select("#answer1")
+        .append("path")
+        .attr("d", line(dataset))
+        .attr("stroke", "#2e2928")
+
+});
+
